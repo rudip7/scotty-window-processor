f56d5fb27a4eb79321c0280d93669fc9485f3c17
zahra196 <zahra2salmani@gmail.com>
2019-11-28 16:41:00 +0100

    28-11-2019e

--- EDADS/src/main/java/Tests/FiFoSamplerTest.java
+++ EDADS/src/main/java/Tests/FiFoSamplerTest.java
@@ -20,7 +20,7 @@ public class FiFoSamplerTest {
         String fileName = "data/testdata.csv";
         File file = new File(fileName);
 
-        // this gives you a 2-dimensional array of strings
+        // read element from files before it exceeds sample size
         LinkedList<String> lines = new LinkedList<>();
         Scanner inputStream;
 
@@ -40,44 +40,108 @@ public class FiFoSamplerTest {
             SampleElement sampleElement=new SampleElement(lines.get(i),i);
             fifoSampler.update(sampleElement);
         }
+        //create desired sample when fifosampler is not full yet
+        SampleElement[] Sample1= new SampleElement[]{new SampleElement("103",0), new SampleElement("52",1),
+                new SampleElement("161",2),
+                new SampleElement("25",3), new SampleElement("188", 4),new SampleElement("19", 5),
+                new SampleElement("48",6),
+                new SampleElement("93",7), new SampleElement("50", 8),new SampleElement("143",9)};
 
-        SampleElement[] Sample1= new SampleElement[]{new SampleElement(103,0), new SampleElement(52,1), new SampleElement(161,2),
-                new SampleElement(25,3), new SampleElement(188, 4),new SampleElement(19, 5),new SampleElement(48,6),
-                        new SampleElement(93,7), new SampleElement(50, 8),new SampleElement(143,9)};
+
          TreeSet<SampleElement> notFullExpectedSample = new TreeSet<>();
          for (SampleElement el : Sample1) {
              notFullExpectedSample.add(el);
          }
 
-         Object notFullSample = fifoSampler.getSample();
-        Assert.assertEquals(notFullSample, notFullExpectedSample);
+         TreeSet<SampleElement>  notFullSample = fifoSampler.getSample();
+         // compare desired and actual samples
+         Assertions.assertTrue(sampleTreeSetComprator(notFullSample,notFullExpectedSample));
 
+
+         //the same steps as above but this time read more elements than sample size, so sampler will remove older elements
         for (int i=10;i<lines.size();i++) {
             SampleElement sampleElement=new SampleElement(lines.get(i),i);
             fifoSampler.update(sampleElement);
         }
          TreeSet<SampleElement> fullSample = fifoSampler.getSample();
-
-         SampleElement[] Sample2= new SampleElement[]{new SampleElement(87,90), new SampleElement(198,91), new SampleElement(106,92),
-                 new SampleElement(34,93), new SampleElement(6, 94),new SampleElement(153, 95),new SampleElement(168,96),
-                 new SampleElement(90,97), new SampleElement(55, 98),new SampleElement(59,99)};
+         // desired full sample
+         SampleElement[] Sample2= new SampleElement[]{new SampleElement("87",90), new SampleElement("198",91),
+                 new SampleElement("106",92),
+                 new SampleElement("34",93), new SampleElement("6", 94),
+                 new SampleElement("153", 95),new SampleElement("168",96),
+                 new SampleElement("90",97), new SampleElement("55", 98),new SampleElement("59",99)};
          TreeSet<SampleElement> fullExpectedSample = new TreeSet<>();
          for (SampleElement el : Sample2) {
              fullExpectedSample.add(el);
          }
 
+        Assertions.assertTrue(sampleTreeSetComprator(fullExpectedSample,fullSample));
 
-        Assert.assertEquals(fullSample, fullExpectedSample);
-    }
+     }
 
     @Test
     public void mergeTest() throws Exception {
-        FiFoSampler fifoSampler = new FiFoSampler(12);
+        FiFoSampler fifoSampler = new FiFoSampler(10);
+        FiFoSampler otherWithSize = new FiFoSampler(12);
+        Assertions.assertThrows(IllegalArgumentException.class,()->fifoSampler.merge(otherWithSize));
+
         FiFoSampler other = new FiFoSampler(10);
-        Assertions.assertThrows(IllegalArgumentException.class,()->fifoSampler.merge(other));
+        SampleElement[] Sample1= new SampleElement[]{new SampleElement("87",1), new SampleElement("198",5),
+                new SampleElement("106",11),
+                new SampleElement("34",12), new SampleElement("6", 17),
+                new SampleElement("153", 20),new SampleElement("168",23),
+                new SampleElement("90",24), new SampleElement("55", 28),new SampleElement("59",30)};
+
+        for (SampleElement el : Sample1) {
+            fifoSampler.update(el);
+        }
+
+        SampleElement[] Sample2= new SampleElement[]{new SampleElement("87",3), new SampleElement("198",6),
+                new SampleElement("106",8),
+                new SampleElement("34",13), new SampleElement("6", 19),
+                new SampleElement("153", 22),new SampleElement("168",25),
+                new SampleElement("90",29), new SampleElement("55", 31),new SampleElement("59",32)};
+
+        for (SampleElement el : Sample2) {
+            other.update(el);
+        }
+
+        SampleElement[] mergeExpectedSample= new SampleElement[]{new SampleElement("87",3), new SampleElement("87",3),
+                new SampleElement("198",5),
+                new SampleElement("198",6), new SampleElement("106", 8),
+                new SampleElement("106", 11),new SampleElement("34",12),
+                new SampleElement("34",13), new SampleElement("6", 17),new SampleElement("6",19)};
+        TreeSet<SampleElement> mergeExpectedTree = new TreeSet<>();
+        for (SampleElement el : mergeExpectedSample) {
+            mergeExpectedTree.add(el);
+        }
+        TreeSet<SampleElement> mergeTree = fifoSampler.merge(other).getSample();
+        System.out.println(mergeExpectedTree);
+        System.out.println(mergeTree);
+        Assertions.assertTrue(sampleTreeSetComprator(mergeTree,mergeExpectedTree));
+
     }
 
+   public boolean sampleTreeSetComprator(TreeSet<SampleElement> tree1,TreeSet<SampleElement> tree2)
+   {
+       boolean Result=true;
 
+       SampleElement element1, element2;
+       if(tree1.size()!=tree2.size())
+       {
+           Result=false;
+       }
+       while(!tree1.isEmpty()&&!tree2.isEmpty()){
+
+           element1= tree1.pollFirst();
+           element2= tree2.pollFirst();
+           if(((String)element1.getValue()).compareTo((String) element2.getValue()) != 0)
+           {
+               Result=false;
+           }
+       }
+       return Result;
+   }
 
 }
 
