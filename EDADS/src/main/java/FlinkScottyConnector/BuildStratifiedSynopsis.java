c25f3132189f9e9b37e6e671c5bd5da6c6db2a52
Joscha von Hein <joscha.von.hein@gmail.com>
2020-05-18 10:55:14 +0200

    empty commit to let me merge again (properly)

--- EDADS/src/main/java/FlinkScottyConnector/BuildStratifiedSynopsis.java
+++ EDADS/src/main/java/FlinkScottyConnector/BuildStratifiedSynopsis.java
@@ -21,6 +21,9 @@ import org.apache.flink.util.Collector;
 import javax.annotation.Nullable;
 import java.util.PriorityQueue;
 
+
+// simple comment to let me commit for new merge
+
 /**
  * Class to organize the static functions to generate window based Synopses.
  *
